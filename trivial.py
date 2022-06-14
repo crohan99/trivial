9b50a797a134b9162274dbac405307b2c2bc72f4
Carson Rohan <crohan@localhost.localdomain>
2022-06-13 23:24:27 -0500

    Added game loop for continuously asking questions until end command is invoked, formatted questions and answers for readability

--- trivial.py
+++ trivial.py
@@ -6,16 +6,12 @@ from twitchio.ext import commands
 TRIVIA_API_URL = 'https://opentdb.com/api.php?amount=10'
 TRIVIA_API_PARAMS = {'prompt':'answer'}
 
-class Bot(commands.Bot):
+class TriviaBot(commands.Bot):
     user = ''
     prompt = ''
     answer = ''
-    incorrect_answers = {}
+    candidates = {}
     trivia_started = False
-    options = {0: 'a',
-               1: 'b',
-               2: 'c',
-               3: 'd'}
 
     def __init__(self):
         # init bot
@@ -25,57 +21,80 @@ class Bot(commands.Bot):
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
 
-    async def event_message(self, message, ctx: commands.Context):
-        # Messages with echo set to true are messages sent by the bot
-        # which we want to ignore.
+    # main loop
+    async def event_message(self, message):
+        # messages with echo set to true are from the bot (we want to ignore those)
         if(message.echo):
             return
 
+        # blocking?
         await self.handle_commands(message)
 
-        if(self.trivia_started):
-                   
+        if(not self.trivia_started): return
+
+        if(not self.user == message.author.name): return
+
+        if(message.content.lower() == 'a' or message.content.lower() == 'b' or
+            message.content.lower() == 'c' or message.content.lower() == 'd'):
+
+            if(message.content.lower() == self.answer):
+                await message.channel.send(f'Correct')
+
+            else:
+                await message.channel.send(f'Incorrect. Correct answer was: {self.answer}. {self.candidates[self.answer]}')
+
+            # ctx = commands.Context(message, commands.Bot)
+            ctx = await self.get_context(message=message)
+            await self.run_trivia(ctx)
+        else:
+            return
 
     # here's where we handle the !trivia command
     @commands.command(name = 'trivia')
-    async def trivia(self, ctx: commands.Context):
-        endTrivia = False
-        self.user = ctx.author.name
+    async def cmd_trivia(self, ctx: commands.Context):
+        if(self.trivia_started): return
 
-        # start the game loop, break on !endtrivia
-        while(not self.endtrivia):
-            await self.get_prompts()
-            await self.prompt_user(ctx)
-            await self.eval_response()
+        self.trivia_started = True
+        self.user = ctx.author.name
+        await self.run_trivia(ctx)
 
     @commands.command(name = 'endtrivia')
-    async def endtrivia(self, ctx: commands.Context):
+    async def cmd_endtrivia(self, ctx: commands.Context):
+        if(not self.trivia_started): return
+            
+        self.trivia_started = False
+        await ctx.send(f'{self.user} ended their trivia sesh.')
 
-        if(self.endtrivia):
-            return
-        else:
-            self.endtrivia = True
-
+    # modify this function to work for other data sources
     async def get_prompts(self):
-        rand = randint(0, 9)
+        question_idx = randint(0, 9)
+        answer_idx = randint(0, 3)
+        print(answer_idx)
         r = requests.get(url = TRIVIA_API_URL, params = TRIVIA_API_PARAMS)
         data = r.json()
 
         # store prompt data
-        # print(data['results'][rand]['question'])
-        self.prompt = data['results'][rand]['question']
-        self.answer = data['results'][rand]['correct_answer']
-        self.incorrect_answers = data['results'][rand]['incorrect_answers']
+        self.prompt = data['results'][question_idx]['question']
+        opts = {0: 'a', 1: 'b', 2: 'c', 3: 'd'}
 
-    async def prompt_user(self, ctx):
+        # store answer data mixed up
+        self.candidates[opts[answer_idx]] = data['results'][question_idx]['correct_answer']
+        self.candidates[opts[(answer_idx + 1) % 4]] = data['results'][question_idx]['incorrect_answers'][1]
+        self.candidates[opts[(answer_idx + 2) % 4]] = data['results'][question_idx]['incorrect_answers'][2]
+        self.candidates[opts[(answer_idx + 3) % 4]] = data['results'][question_idx]['incorrect_answers'][0]
+
+        self.answer = opts[answer_idx]
+
+    async def prompt_user(self, ctx: commands.Context):
+        start = randint(0, 3)
         await ctx.send(f'Here is your question, {self.user}: \n'
         f'{self.prompt} \n'
-        f'Is it: \n'
-        f'{self.incorrect_answers}'
-        f'{self.answer}')
+        'a. {a}\nb. {b}\nc. {c}\nd. {d}'.format(**self.candidates))
 
-    async def eval_response(self):
-        return
+    async def run_trivia(self, ctx: commands.Context):
+        await self.get_prompts()
+        await self.prompt_user(ctx)
 
-bot = Bot()
-bot.run()
+
+tBot = TriviaBot()
+tBot.run()
