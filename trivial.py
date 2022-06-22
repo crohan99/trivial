9872a00a4bd1e90e9c73c652b99772c0c4af86c4
Carson Rohan <crohan@localhost.localdomain>
2022-06-21 19:19:37 -0500

    Automagically get access token

--- trivial.py
+++ trivial.py
@@ -1,31 +1,31 @@
-import requests
+import asyncio
 import json
 import time
-import datetime
+import threading
+import http.client
 from random import randint
 from twitchio.ext import commands
 
 TRIVIA_API_URL = 'https://opentdb.com/api.php?amount=10'
 TRIVIA_API_PARAMS = {'prompt':'answer'}
-TIMEOUT = 30
+TIMEOUT = 1
 
 class TriviaBot(commands.Bot):
-    adminName: str
-    userName: str
-    prompt: str
-    answer: str
-    candidates: dict
-    trivia_started: bool
-    starttime: float
+    adminName = ''
+    userName = ''
+    prompt = ''
+    answer = ''
+    incorrect_answers = {}
+    start_time = 0.0
+    trivia_started = False
 
     def __init__(self):
         f = open('config.json', "r")
         data = json.loads(f.read())
-        print(data['secret'])
-        print(data['channel'])
-        print(data['adminName'])
 
-        self.adminName = data['adminName']
+        self.connect()
+
+        self.trivia_started = False
         # init bot
         # super().__init__(token='zj3nhpbiun93gav6e4lth774qum4ql', prefix='!', initial_channels=['motorslam99'])
         super().__init__(token=data['secret'], prefix='!', initial_channels=[data['channel']])
@@ -47,17 +47,15 @@ class TriviaBot(commands.Bot):
 
         if(not self.userName == message.author.name): return
 
-        if(round((time.time() - self.starttime)) >= TIMEOUT): 
-            self.trivia_started = False
-            await message.channel.send(f'Trivia ended, {self.userName}: you ran out of time!')
-
         if(message.content.lower() == 'a' or message.content.lower() == 'b' or
             message.content.lower() == 'c' or message.content.lower() == 'd'):
 
             if(message.content.lower() == self.answer):
                 await message.channel.send(f'Correct')
             else:
-                await message.channel.send(f'Incorrect. Correct answer was: {self.answer}. {self.candidates[self.answer]}')
+                await message.channel.send(f'Incorrect. Correct answer was: {self.answer}. {self.incorrect_answers[self.answer]}')
+
+            self.trivia_started = False
 
             # ctx = await self.get_context(message=message)
             # await self.run_trivia(ctx)
@@ -71,7 +69,9 @@ class TriviaBot(commands.Bot):
 
         self.trivia_started = True
         self.userName = ctx.author.name
-        self.starttime = time.time()
+        self.start_time = time.time()
+        timer_thread = threading.Thread(target=self.timer_handler, args=(ctx,))
+        timer_thread.start()
         await self.run_trivia(ctx)
 
     @commands.command(name = 'endtrivia')
@@ -85,36 +85,60 @@ class TriviaBot(commands.Bot):
 
     # modify this function to work for other data sources
     async def get_prompts(self):
-        # question_idx = randint(0, 9)
-        
-        # r = requests.get(url = TRIVIA_API_URL, params = TRIVIA_API_PARAMS)
-        # data = r.json()
-
         f = open('config.json', "r")
         data = json.loads(f.read())
 
         answer_idx = randint(0, 3)
-        question_idx = randint(0, data.keys())
-        
+        question_idx = randint(0, len(data['questions']) - 1)
+        print(question_idx)
+
         # store prompt data
         self.prompt = data['questions'][question_idx]['question']
         opts = {0: 'a', 1: 'b', 2: 'c', 3: 'd'}
 
         # store answer data mixed up
-        self.candidates[opts[answer_idx]] = data['questions'][question_idx]['correct_answer']
-        self.candidates[opts[(answer_idx + 1) % 4]] = data['questions'][question_idx]['incorrect_answers'][1]
-        self.candidates[opts[(answer_idx + 2) % 4]] = data['questions'][question_idx]['incorrect_answers'][2]
-        self.candidates[opts[(answer_idx + 3) % 4]] = data['questions'][question_idx]['incorrect_answers'][0]
+        self.incorrect_answers[opts[answer_idx]] = data['questions'][question_idx]['correct_answer']
+        self.incorrect_answers[opts[(answer_idx + 1) % 4]] = data['questions'][question_idx]['incorrect_answers'][1]
+        self.incorrect_answers[opts[(answer_idx + 2) % 4]] = data['questions'][question_idx]['incorrect_answers'][2]
+        self.incorrect_answers[opts[(answer_idx + 3) % 4]] = data['questions'][question_idx]['incorrect_answers'][0]
         self.answer = opts[answer_idx]
 
     async def prompt_user(self, ctx: commands.Context):
         await ctx.send(f'Here is your question, {self.userName}: \n'
         f'{self.prompt} \n'
-        'a. {a}\nb. {b}\nc. {c}\nd. {d}'.format(**self.candidates))
+        'a. {a}\nb. {b}\nc. {c}\nd. {d}'.format(**self.incorrect_answers))
 
     async def run_trivia(self, ctx: commands.Context):
         await self.get_prompts()
         await self.prompt_user(ctx)
+
+    async def timer(self, ctx: commands.Context):
+        while(self.trivia_started):
+
+            if(round((time.time() - self.start_time)) >= TIMEOUT):
+                self.trivia_started = False
+                await ctx.send(f'{self.userName} ran out of time!')
+
+    def timer_handler(self, ctx: commands.Context):
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+        loop.run_until_complete(self.timer(ctx))
+        loop.close()
+
+    def connect(self):
+        f = open('config.json', "r")
+        data = json.loads(f.read())
+
+        connection = http.client.HTTPSConnection('')
+        payload = 'grant_type=client_credentials&client_id=' + data['clientID'] + '&client_secret=' + data['secret']
+        headers = {'content-type': "application/x-www-form-urlencoded"}
+        connection.request("POST", 'https://id.twitch.tv/oauth2/token', payload, headers)
+
+        res = connection.getresponse()
+        data = json.loads(res.read())
+        print(data['access_token'])
+        return data['access_token']
 
 
 tBot = TriviaBot()
